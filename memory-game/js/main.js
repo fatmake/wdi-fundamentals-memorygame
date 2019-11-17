c20aab48f4503291b315e2f1033be9ad2f4e6fc8
fatmake <fatmakelantan@outlook.com>
2019-11-17 05:40:41 +0300

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,31 +1,32 @@
-console.log("Up and running!");
 
-/*var cardOne = "qween";
-var cardTwo = "qween";
-var cardThree = "king";
-var cardFour = "king";*/
-
-//console.log("User flipped "+ cardOne);
 
 var cards = ["qween","qween","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
+var cardId;
 
-cardsInPlay.push(cardOne);
-console.log("User flipped "+ cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped "+ cardTwo);
+var checkForMatch = function() {
 
-if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  		console.log("You found a match!");
+	} 	else {
+  			console.log("Sorry, try again.");
+			}
+} 
 
-	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	}
+var flipCard = function(cardId){
 
-	else {
-		alert("Sorry, try again.");
+
+	console.log("User flipped "+ cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length === 2){
+		checkForMatch();
 	}
 
 
 }
+
+flipCard(0);
+flipCard(2);
+
+
